441ddb050ac9f2c5e884fdd41ffdb43dce8b0a7d
Can Altay <canaltay2001@windowslive.com>
2026-09-13 00:01:07 +0300

    Update JanBehaviour.cs

--- JanBehaviour.cs
+++ JanBehaviour.cs
@@ -7,15 +7,10 @@ namespace Jan.Core
 {
     public abstract class JanBehaviour : MonoBehaviour
     {
-        public new Transform transform { get; private set; }
-        public new GameObject gameObject { get; private set; }
-
         [SerializeField] protected bool overrideLayer = true;  
 
         protected virtual void OnEnable()
         {
-            transform = base.transform;
-            gameObject = base.gameObject;
 
             if(this is IInteractable interactable && overrideLayer)
             {
@@ -31,15 +26,10 @@ namespace Jan.Core
 
     public abstract class JanBehaviour<T> : Singleton<T> where T : SerializedMonoBehaviour
     {
-        public new Transform transform { get; private set; }
-        public new GameObject gameObject { get; private set; }
         [SerializeField] private bool overrideLayer = true;
 
         protected virtual void OnEnable()
         {
-            transform = base.transform;
-            gameObject = base.gameObject;
-
             if(this is IInteractable interactable && overrideLayer)
             {
                 gameObject.SetLayerToChildren(Layers.Interactable);
